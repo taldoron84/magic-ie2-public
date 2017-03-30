22545293e1db043209088bc1f22af8b7664fc3da
Tal Doron <tald@gigaspaces.com>
2017-03-30 23:52:06 +0300

    Change template type to sql query

--- events-space/src/main/java/com/magic/CarEventProcessor.java
+++ events-space/src/main/java/com/magic/CarEventProcessor.java
@@ -10,26 +10,31 @@ import org.openspaces.events.EventDriven;
 import org.openspaces.events.EventTemplate;
 import org.openspaces.events.adapter.SpaceDataEvent;
 import org.openspaces.events.polling.Polling;
+import com.j_spaces.core.client.SQLQuery;
+
 
 import com.magic.insightedge.model.CarEvent;
 
 
-@EventDriven @Polling
+@EventDriven
+@Polling
 public class CarEventProcessor {
 
     @EventTemplate
-    CarEvent unprocessedData() {
-        CarEvent template = new CarEvent();
-        template.setIsSentByHttp(false);
+    SQLQuery<CarEvent> unprocessedData() {
+        SQLQuery<CarEvent> template = new SQLQuery<CarEvent>(CarEvent.class, "IsSentByHttp = false");
         return template;
+//        CarEvent template = new CarEvent();
+//        template.setIsSentByHttp(false);
+//        return template;
     }
 
     @SpaceDataEvent
     public void eventListener(CarEvent event) {
         // Call external HTTP
-    	System.out.println("Calling HTTP client for car event: " + event.getID());
-    	try {
-    		URL url = new URL("http://localhost:8091/");
+        System.out.println("Calling HTTP client for car event: " + event.getID());
+        try {
+            URL url = new URL("http://localhost:8091/v1");
             URLConnection conn = url.openConnection();
             conn.setDoOutput(true);
             OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
@@ -39,14 +44,12 @@ public class CarEventProcessor {
             String line;
             BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             while ((line = reader.readLine()) != null) {
-              System.out.println(line);
+                System.out.println(line);
             }
             writer.close();
             reader.close();
-    	} catch(Exception e) {
-    		
-    	}
-    	
-    	
+        } catch (Exception e) {
+
+        }
     }
 }
